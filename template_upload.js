9b63e146d2ec54c843ae5359cfef226e19fa8cc4
staitdarren <staitdarren@gmail.com>
2026-02-03 15:11:39 +0000

    Add files via upload

---
+++ template_upload.js
@@ -0,0 +1,106 @@
+const QUESTION_POOL = {
+  A: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  B: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  C: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  D: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  E: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  F: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  G: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  H: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  I: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  J: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  K: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  L: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  M: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  N: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  O: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  P: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  Q: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  R: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  S: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  T: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  U: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  V: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  W: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  X: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  Y: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ],
+  Z: [
+    { question: "Placeholder question", answer: "Placeholder answer" },
+    { question: "Placeholder question2", answer: "Placeholder answer2" }
+  ]
+};
