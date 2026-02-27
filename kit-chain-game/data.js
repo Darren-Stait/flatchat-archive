4cfab69075c895512af804495a4c0146e4da2b9c
staitdarren <staitdarren@gmail.com>
2026-02-27 22:09:52 +0000

    Create data.js

---
+++ kit-chain-game/data.js
@@ -0,0 +1,190 @@
+const GAME_DATA = [
+  {
+    club: "Chelsea",
+    badge: "https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg",
+    primaryColor: "#001489",
+    kitNumbers: [
+      {
+        number: "9",
+        difficulty: "easy",
+        players: [
+          {
+            name: "Gianluca Vialli",
+            years: "1996-1999",
+            sponsor: "./sponsors/autoglass.png"
+          },
+          {
+            name: "Chris Sutton",
+            years: "1999-2000",
+            sponsor: "./sponsors/autoglass.png"
+          },
+          {
+            name: "Jimmy Floyd Hasselbaink",
+            years: "2000–2004",
+            sponsor: "./sponsors/emirates.png"
+          },
+          {
+            name: "Mateja Kezman",
+            years: "2004-2005",
+            sponsor: "./sponsors/samsung.png"
+          },
+          {
+            name: "Hernan Crespo",
+            years: "2005-2006",
+            sponsor: "./sponsors/samsung.png"
+          },
+          {
+            name: "Khalid Boulahrouz",
+            years: "2006-2007",
+            sponsor: "./sponsors/samsung.png"
+          },
+          {
+            name: "Steve Sidwell",
+            years: "2007-2008",
+            sponsor: "./sponsors/samsung.png"
+          },
+          {
+            name: "Franco Di Santo",
+            years: "2008-2010",
+            sponsor: "./sponsors/samsung.png"
+          },
+          {
+            name: "Fernando Torres",
+            years: "2011–2015",
+            sponsor: "./sponsors/samsung.png"
+          },
+          {
+            name: "Radamel Falcao",
+            years: "2015–2016",
+            sponsor: "./sponsors/yokohoma.png"
+          },
+          {
+            name: "Alvaro Morata",
+            years: "2017-2018",
+            sponsor: "./sponsors/yokohoma.png"
+          },
+          {
+            name: "Gonzalo Higuain",
+            years: "2018-2019",
+            sponsor: "./sponsors/yokohoma.png"
+          },
+          {
+            name: "Tammy Abraham",
+            years: "2019-2021",
+            sponsor: "./sponsors/yokohoma.png"
+          },
+          {
+            name: "Romelu Lukaku",
+            years: "2021-2022",
+            sponsor: "./sponsors/three.png"
+          },
+          {
+            name: "Pierre-Emerick Aubameyang",
+            years: "2022-2023",
+            sponsor: "./sponsors/three.png"
+          },
+          {
+            name: "Liam Delap",
+            years: "2024-2025",
+            sponsor: "./sponsors/ifs.png"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    club: "Manchester United",
+    badge: "https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg",
+    primaryColor: "#c4030c",
+    kitNumbers: [
+      {
+        number: "7",
+        sponsor: "https://upload.wikimedia.org/wikipedia/commons/4/4c/TeamViewer_Logo.svg",
+        difficulty: "easy",
+        players: [         
+          {
+            name: "Andrey Kanchelskis",
+            years: "1991-1992",
+            sponsor: "./sponsors/sharp.png"
+          },
+          {
+            name: "Eric Cantona",
+            years: "1992-1993",
+            sponsor: "./sponsors/sharp.png"
+          },              
+          {
+            name: "Bryan Robson",
+            years: "1993-1994",
+            sponsor: "./sponsors/sharp.png"
+          },            
+          {
+            name: "Andrey Kanchelskis",
+            years: "1994-1995",
+            sponsor: "./sponsors/sharp.png"
+          },
+          {
+            name: "David Beckham",
+            years: "1995-1996",
+            sponsor: "./sponsors/sharp.png"
+          }, 
+          {
+            name: "Eric Cantona",
+            years: "1996-1997",
+            sponsor: "./sponsors/sharp.png"
+          },             
+          {
+            name: "David Beckham",
+            years: "1997-2003",
+            sponsor: "./sponsors/sharp.png"
+          },            
+          {
+            name: "Cristiano Ronaldo",
+            years: "2003-2009",
+            sponsor: "./sponsors/vodafone.png"
+          },            
+          {
+            name: "Michael Owen",
+            years: "2009-2012",
+            sponsor: "./sponsors/aon.png"
+          },
+          {
+            name: "Antonio Valencia",
+            years: "2014-2015",
+            sponsor: "./sponsors/chevrolet.png"
+          },
+          {
+            name: "Ángel Di María",
+            years: "2014-2015",
+            sponsor: "./sponsors/chevrolet.png"
+          },
+          {
+            name: "Memphis Depay",
+            years: "2015-2017",
+            sponsor: "./sponsors/chevrolet.png"
+          },
+          {
+            name: "Alexis Sanchez",
+            years: "2017-2019",
+            sponsor: "./sponsors/chevrolet.png"
+          },
+          {
+            name: "Edinson Cavani",
+            years: "2020-2021",
+            sponsor: "./sponsors/chevrolet.png"
+          },
+          {
+            name: "Cristiano Ronaldo",
+            years: "2021-2023",
+            sponsor: "./sponsors/teamviewer.png"
+          },  
+          {
+            name: "Mason Mount",
+            years: "2023-2026",
+            sponsor: "./sponsors/snapdragon.png"
+          },
+
+        ]
+      }
+    ]
+  }
+];
