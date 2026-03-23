714c3665a0b69f8b10a6140c9d57b0ef6381b7ff
staitdarren <staitdarren@gmail.com>
2026-03-23 14:38:44 +0000

    Update data.js

--- kit-chain-game/data.js
+++ kit-chain-game/data.js
@@ -3,7 +3,73 @@ const GAME_DATA = [
     club: "Chelsea",
     badge: "https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg",
     primaryColor: "#001489",
+    sponsors: [
+      { start: 1987, end: 1993, image: "commodore.png" },
+      { start: 1993, end: 1994, image: "amiga.png" },
+      { start: 1994, end: 1997, image: "coors.png" },
+      { start: 1997, end: 2001, image: "autoglass.png" },
+      { start: 2001, end: 2005, image: "emirates.png" },
+      { start: 2005, end: 2015, image: "samsung.png" },
+      { start: 2015, end: 2020, image: "yokohoma.png" },
+      { start: 2020, end: 2023, image: "three.png" },
+      { start: 2023, end: 2024, image: "infiniteathlete.png" },
+      { start: 2025, end: 2026, image: "ifs.png" }
+    ],
     kitNumbers: [
+      {
+        number: "1",
+        difficulty: "easy",
+        players: [
+          {
+            name: "Dave Beasant",
+            years: "1989-1993",
+            nationality: "England",
+            position: "Goalkeeper"
+          },
+          {
+            name: "Dmitriy Kharine",
+            years: "1993-1997",
+            nationality: "Russia",
+            position: "Goalkeeper"
+          },
+          {
+            name: "Ed De Goey",
+            years: "1997-2003",
+            nationality: "Netherlands",
+            position: "Goalkeeper"
+          },
+          {
+            name: "Petr Cech",
+            years: "2004-2015",
+            nationality: "Czech Republic",
+            position: "Goalkeeper"
+          },
+          {
+            name: "Asmir Begovic",
+            years: "2015-2017",
+            nationality: "Bosnia and Herzegovina",
+            position: "Goalkeeper"
+          },
+          {
+            name: "Willy Caballero",
+            years: "2017-2018",
+            nationality: "Argentina",
+            position: "Goalkeeper"
+          },
+          {
+            name: "Kepa Arrizabalaga",
+            years: "2018-2023",
+            nationality: "Spain",
+            position: "Goalkeeper"
+          },
+          {
+            name: "Robert Sanchez",
+            years: "2023-2026",
+            nationality: "Spain",
+            position: "Goalkeeper"
+          }
+        ]
+      },
       {
         number: "9",
         difficulty: "easy",
@@ -11,115 +77,183 @@ const GAME_DATA = [
           {
             name: "Gianluca Vialli",
             years: "1996-1999",
-            sponsor: "./sponsors/autoglass.png",
             nationality: "Italy",
             position: "Striker"
           },
           {
             name: "Chris Sutton",
             years: "1999-2000",
-            sponsor: "./sponsors/autoglass.png",
             nationality: "England",
             position: "Striker"
           },
           {
             name: "Jimmy Floyd Hasselbaink",
             years: "2000–2004",
-            sponsor: "./sponsors/emirates.png",
             nationality: "Netherlands",
             position: "Striker"
           },
           {
             name: "Mateja Kezman",
             years: "2004-2005",
-            sponsor: "./sponsors/samsung.png",
             nationality: "Serbia",
             position: "Striker"
           },
           {
             name: "Hernan Crespo",
             years: "2005-2006",
-            sponsor: "./sponsors/samsung.png",
             nationality: "Argentina",
             position: "Striker"
           },
           {
             name: "Khalid Boulahrouz",
             years: "2006-2007",
-            sponsor: "./sponsors/samsung.png",
             nationality: "Netherlands",
             position: "Defender"
           },
           {
             name: "Steve Sidwell",
             years: "2007-2008",
-            sponsor: "./sponsors/samsung.png",
             nationality: "England",
             position: "Midfielder"
           },
           {
             name: "Franco Di Santo",
             years: "2008-2010",
-            sponsor: "./sponsors/samsung.png",
             nationality: "Argentina",
             position: "Striker"
           },
           {
             name: "Fernando Torres",
             years: "2011–2015",
-            sponsor: "./sponsors/samsung.png",
             nationality: "Spain",
             position: "Striker"
           },
           {
             name: "Radamel Falcao",
             years: "2015–2016",
-            sponsor: "./sponsors/yokohoma.png",
             nationality: "Colombia",
             position: "Striker"
           },
           {
             name: "Alvaro Morata",
             years: "2017-2018",
-            sponsor: "./sponsors/yokohoma.png",
             nationality: "Spain",
             position: "Striker"
           },
           {
             name: "Gonzalo Higuain",
             years: "2018-2019",
-            sponsor: "./sponsors/yokohoma.png",
             nationality: "Argentina",
             position: "Striker"
           },
           {
             name: "Tammy Abraham",
             years: "2019-2021",
-            sponsor: "./sponsors/yokohoma.png",
             nationality: "England",
             position: "Striker"
           },
           {
             name: "Romelu Lukaku",
             years: "2021-2022",
-            sponsor: "./sponsors/three.png",
             nationality: "Belgium",
             position: "Striker"
           },
           {
             name: "Pierre-Emerick Aubameyang",
             years: "2022-2023",
-            sponsor: "./sponsors/three.png",
             nationality: "Gabon",
             position: "Striker"
           },
           {
             name: "Liam Delap",
             years: "2024-2025",
-            sponsor: "./sponsors/ifs.png",
             nationality: "England",
             position: "Striker"
           }
+        ]
+      },
+            {
+        number: "11",
+        difficulty: "easy",
+        players: [
+          {
+            name: "Dennise Wise",
+            years: "1990-1999",
+            nationality: "England",
+            position: "Midfielder"
+          },
+          {
+            name: "Boudewijn Zenden",
+            years: "2001-2003",
+            nationality: "Netherlands",
+            position: "Winger"
+          },
+          {
+            name: "Damien Duff",
+            years: "2003-2006",
+            nationality: "Ireland",
+            position: "Winger"
+          },
+          {
+            name: "Didier Drogba",
+            years: "2006-2012",
+            nationality: "Ivory Coast",
+            position: "Striker"
+          },
+          {
+            name: "Oscar",
+            years: "2012-2014",
+            nationality: "Brazil",
+            position: "Attacking Midfielder"
+          },
+          {
+            name: "Didier Drogba",
+            years: "2014-2015",
+            nationality: "Ivory Coast",
+            position: "Striker"
+          },
+          {
+            name: "Juan Cuadrado",
+            years: "2015-2016",
+            nationality: "Colombia",
+            position: "Winger"
+          },
+          {
+            name: "Alexandre Pato",
+            years: "2015-2016",
+            nationality: "Brazil",
+            position: "Striker"
+          },
+          {
+            name: "Pedro",
+            years: "2016-2020",
+            nationality: "Spain",
+            position: "Winger"
+          },
+          {
+            name: "Timo Werner",
+            years: "2020-2022",
+            nationality: "Germany",
+            position: "Striker"
+          },
+          {
+            name: "Joao Felix",
+            years: "2022-2023",
+            nationality: "Portugal",
+            position: "Attacking Midfielder"
+          },
+          {
+            name: "Noni Madueke",
+            years: "2023-2025",
+            nationality: "England",
+            position: "Winger"
+          },
+          {
+            name: "Jamie Gittens",
+            years: "2025-2026",
+            nationality: "England",
+            position: "Winger"
+          },
         ]
       }
     ]
@@ -128,6 +262,15 @@ const GAME_DATA = [
     club: "Manchester United",
     badge: "https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg",
     primaryColor: "#c4030c",
+    sponsors: [
+      { start: 1982, end: 2000, image: "sharp.png" },
+      { start: 2000, end: 2006, image: "vodafone.png" },
+      { start: 2006, end: 2010, image: "aig.png" },
+      { start: 2010, end: 2014, image: "aon.png" },
+      { start: 2014, end: 2021, image: "chevrolet.png" },
+      { start: 2021, end: 2024, image: "teamviewer.png" },
+      { start: 2024, end: 2026, image: "snapdragon.png" }
+    ],
     kitNumbers: [
       {
         number: "7",
@@ -137,95 +280,81 @@ const GAME_DATA = [
           {
             name: "Lee Sharpe",
             years: "1990-1991",
-            sponsor: "./sponsors/sharp.png",
             nationality: "England",
             position: "Winger"
           },      
           {
             name: "Andrey Kanchelskis",
             years: "1991-1992",
-            sponsor: "./sponsors/sharp.png",
             nationality: "Russia",
             position: "Winger"
           },
           {
             name: "Eric Cantona",
             years: "1992-1997",
-            sponsor: "./sponsors/sharp.png",
             nationality: "France",
             position: "Striker"
           },                          
           {
             name: "David Beckham",
             years: "1997-2003",
-            sponsor: "./sponsors/sharp.png",
             nationality: "England",
             position: "Midfielder/Winger"
           },            
           {
             name: "Cristiano Ronaldo",
             years: "2003-2009",
-            sponsor: "./sponsors/vodafone.png",
             nationality: "Portugal",
             position: "Winger"
           },            
           {
             name: "Michael Owen",
             years: "2009-2012",
-            sponsor: "./sponsors/aon.png",
             nationality: "England",
             position: "Striker"
           },
           {
             name: "Antonio Valencia",
             years: "2012-2014",
-            sponsor: "./sponsors/chevrolet.png",
             nationality: "Ecuador",
             position: "Winger/Fullback"
           },
           {
             name: "Ángel Di María",
             years: "2014-2015",
-            sponsor: "./sponsors/chevrolet.png",
             nationality: "Argentina",
             position: "Winger/Midfielder"
           },
           {
             name: "Memphis Depay",
             years: "2015-2017",
-            sponsor: "./sponsors/chevrolet.png",
             nationality: "Netherlands",
             position: "Winger"
           },
           {
             name: "Alexis Sanchez",
             years: "2017-2019",
-            sponsor: "./sponsors/chevrolet.png",
             nationality: "Chile",
             position: "Winger"
           },
           {
             name: "Edinson Cavani",
             years: "2020-2021",
-            sponsor: "./sponsors/chevrolet.png",
             nationality: "Uruguay",
             position: "Striker"
           },
           {
             name: "Cristiano Ronaldo",
             years: "2021-2023",
-            sponsor: "./sponsors/teamviewer.png",
             nationality: "Portugal",
             position: "Striker"
           },  
           {
             name: "Mason Mount",
             years: "2023-2026",
-            sponsor: "./sponsors/snapdragon.png",
             nationality: "England",
             position: "Midfielder"
           },
-
         ]
       }
     ]
